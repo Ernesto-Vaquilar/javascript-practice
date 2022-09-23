0782a6d6327729488fe0bf34a85ceb5d5ef08178
Ernesto-Vaquilar <vaquilar.ernesto@gmail.com>
2022-09-22 21:22:48 -0700

    feat: Print a number, BigInt, and their types

--- js/script.js
+++ js/script.js
@@ -10,4 +10,11 @@ console.log("Ernesto");
 
 console.log("chilaquiles");
 
+// day-2 daily challenge
 console.log("Supercalifragilisticexpialidocious".length);
+
+console.log(543789);
+console.log(typeof 543789);
+
+console.log(123478097534217809743217809234587432178064108974321709n);
+console.log(typeof 123478097534217809743217809234587432178064108974321709n);
